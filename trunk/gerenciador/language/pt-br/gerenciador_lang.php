8bb96150d5b4dae9f62279a0173675b62c363d79
kmiksi@gmail.com <kmiksi@gmail.com@804fdf4f-70b5-7deb-b165-db2bf7423d30>
2012-09-06 19:22:38 +0000

    correções de conflitos

--- trunk/gerenciador/language/pt-br/gerenciador_lang.php
+++ trunk/gerenciador/language/pt-br/gerenciador_lang.php
@@ -9,11 +9,19 @@ $lang['gerenciadorRelatorioTab1'] = 'Relatório';
 $lang['gerenciadorRelatorioModulosAcesso'] = 'Módulos com acesso';
 $lang['gerenciadorRelatorioNomeNaoInformado'] = 'Nome deve ser informado';
 $lang['gerenciadorRelatorioLinkNaoInformado'] = 'Link deve ser informado';
+$lang['restricaoFiltro'] = 'Filtro';
+$lang['restricaoNome'] = 'Nome';
+$lang['restricaoListaRestricoes'] = 'Lista de restrições';
+$lang['restricaoRestricao'] = 'Restrição';
+$lang['restricaoDescricao'] = 'Descrição';
+$lang['restricaoSQLRestricao'] = 'SQL';
+$lang['restricaoNomeNaoInformado'] = 'Nome deve ser informado';
+$lang['restricaoSQLNaoInformado'] = 'SQL deve ser informado';
 $lang['gerenciadorGeoCodeNaoInformado'] = 'GeoCode deve ser informado';
 
+$lang['usuarioTabRestricoes'] = 'Restrições de dados';
 //--------- PessoaTipo -----------
 $lang['pessoaTipoRequerido'] = 'Você precisa informar o nome do tipo de pessoa';
 $lang['tipoPessoa'] = 'Tipo Pessoa';
 $lang['tipoPessoaTipo'] = 'Tipo';
 $lang['tipoPessoaGrid'] = 'Lista de tipos de pessoas';
-
