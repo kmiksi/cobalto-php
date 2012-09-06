6d9f9d4932011e2d04d4fc928af3fa388399c492
glaucomunsberg@gmail.com <glaucomunsberg@gmail.com@804fdf4f-70b5-7deb-b165-db2bf7423d30>
2012-09-06 16:36:46 +0000

    SQL com função para retorno de parâmetro pelo DB Criado programa PessoaTipo Cidade agora retorna "NomeCidade - SiglaEstado"

--- trunk/gerenciador/models/cidademodel.php
+++ trunk/gerenciador/models/cidademodel.php
@@ -8,5 +8,22 @@ class CidadeModel extends Model {
         $this->db->order_by('nome_cidade', 'asc');
         return $this->db->get('cidades')->result_array();
     }
+    
+    public function getCidadeByNome($nome) {
+        $this->db->select('c.id, c.nome || \' - \' || uf.sigla');
+        $this->db->from('cidades as c');
+        $this->db->join('unidades_federativas as uf','c.unidade_federativa_id = uf.id');
+        if($nome != '')
+            $this->db->like('upper(c.nome)', strtoupper($nome));
+        $this->db->order_by('c.nome', 'asc');
+        $this->db->limit('20');
+        return $this->db->get('')->result();
+    }
+    
+    function getCidadeById($cidadeId){
+	$this->db->where('id', $cidadeId);
+	return $this->db->get('cidades')->row();
+    }
+    
 
 }
