701d9caa4bbceb43fd95920664557c3a7e1d8f90
kmiksi@gmail.com <kmiksi@gmail.com@804fdf4f-70b5-7deb-b165-db2bf7423d30>
2012-12-07 17:54:43 +0000

    Compressão de javascripts e CSSs

--- trunk/static/_views/headerScripts.php
+++ trunk/static/_views/headerScripts.php
@@ -5,6 +5,6 @@
     var IMG = '<?=IMG?>';
     var JS = '<?=JS?>';
 </script>
-<link href='<?=CSS;?>/<?=(@$_COOKIE['tema'] == '' ? 'redmond' : @$_COOKIE['tema']);?>/all.css.jquery-ui.24042012153419492153.css' type='text/css' rel='stylesheet'/>
+<link href='<?=CSS;?>/<?=(@$_COOKIE['tema'] == '' ? 'redmond' : @$_COOKIE['tema']);?>/all.css.jquery-ui.07122012153559413744.css' type='text/css' rel='stylesheet'/>
 <link rel='shortcut icon' href='<?=IMG;?>/favicon.ico' type='image/ico'/>
-<script type='text/javascript' src='<?=JS;?>/all.javascript.24042012153419492153.js'></script>
+<script type='text/javascript' src='<?=JS;?>/all.javascript.07122012153559413744.js'></script>
